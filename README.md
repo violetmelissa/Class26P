833630ef8d1ea7c0449411d2176f4d1ec65ebe9e
violetmelissa <melissa8fon@gmail.com>
2020-08-19 17:25:03 +0530

    i made changes

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # angryBirdsStage2
 Angry Birds stage 2 with Class Inheritance and Images
+hey this is my angry birds game.... and i am working with gitbash
--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ const World= Matter.World;
 const Bodies = Matter.Bodies;
 
 var engine, world;
-var box1, pig1;
+var box1, pig1, platform;
 var backgroundImg;
 
 function preload() {
@@ -17,7 +17,7 @@ function setup(){
 
     
     ground = new Ground(600,height,1200,20)
-
+    platform = new Ground(100,300,400,200)
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
@@ -57,6 +57,6 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
-
+platform.display();
     bird.display();
 }
